3f16473b214ea3d0318cf702b8ca221072f813a1
Dinh Ngoc Hai <dinhngochai196@gmail.com>
2022-12-22 16:43:12 +0700

    change

---
+++ lesson_1/main.js
@@ -0,0 +1,34 @@
+var fullName = 'Dinh Ngoc Hai';
+var age = 26;
+alert(fullName);
+alert(age);
+/**
+ * 
+ * 
+ */
+var nameHai = 'Hai';
+
+console.log(nameHai);
+
+confirm('xac nhan tuoi!');
+
+prompt('xac nhan tuoi');
+
+setTimeout(function () {
+    alert('thong bao')
+}, 1000);
+
+var accNumber = 3 ** 3;
+console.log(accNumber);
+
+// setInterval(function () {
+//     console.log('day la log so: ' + Math.random())
+// }, 3000);
+
+var fullName2 = 'Dinh Ngoc Hai';
+if(fullName2){
+    console.log('dung');
+}
+else{
+    console.log('sai');
+}
