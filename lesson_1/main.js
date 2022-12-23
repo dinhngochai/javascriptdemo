482c4b61f1e8e41694765ba62b95ff7ba2bb049b
HaiNoah <dinhngochai196@gmail.com>
2022-12-23 14:04:32 +0700

    change 2

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -21,9 +21,9 @@ setTimeout(function () {
 var accNumber = 3 ** 3;
 console.log(accNumber);
 
-setInterval(function () {
-    console.log('day la log so: ' + Math.random())
-}, 3000);
+// setInterval(function () {
+//     console.log('day la log so: ' + Math.random())
+// }, 3000);
 
 
 // Dùng dấu \ nếu muốn dùng dấu nháy " ở trong chuỗi string
@@ -37,3 +37,25 @@ if (fullName2) {
 else {
     console.log('sai');
 }
+
+// Function
+var myFunction = function() {
+    alert('Hi, xin chao cac ban')
+}
+
+// Object Type
+var myObject = {
+    name: 'Ngoc Hai',
+    age: 218, 
+    adress: 'Ha Noi',
+    myFunction: function () {
+    }
+}
+console.log('myObject', myObject);
+
+var myArray = [
+    ['arr1', 3, true],
+    [12, 'arr5', function (){}]
+]
+console.log(myArray);
+console.log(typeof myArray);
